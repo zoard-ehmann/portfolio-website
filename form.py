e3d21a58fd1bc2da96b5ffd7897dea5389aff31e
Zoárd Ehmann <zoard.ehmann@gmail.com>
2022-05-29 17:07:20 +0200

    Implementing Login and Portfolio related forms

--- form.py
+++ form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, EmailField, TextAreaField, SubmitField
-from wtforms.validators import InputRequired, Length, Email
+from wtforms import StringField, EmailField, TextAreaField, SubmitField, URLField, PasswordField
+from wtforms.validators import InputRequired, Length, Email, URL
 
 
 class ContactForm(FlaskForm):
@@ -8,3 +8,16 @@ class ContactForm(FlaskForm):
     email = EmailField('Email', validators=[InputRequired(), Email(message='This is not a valid email address.')], render_kw={'placeholder': 'your@email.com'})
     message = TextAreaField('Message', validators=[InputRequired(), Length(min=100, message='Message is too short. Type at least 100 characters.')], render_kw={'placeholder': 'Your message - at least 100 characters'})
     submit = SubmitField('Send')
+
+
+class AddPortfolioItem(FlaskForm):
+    title = StringField('Title', validators=[InputRequired()], render_kw={'placeholder': 'Project Name'})
+    description = TextAreaField('Description', validators=[InputRequired()], render_kw={'placeholder': 'Detailed description about the project...'})
+    url = URLField('Code Repository', validators=[InputRequired(), URL(message='This is not a valid URL.')], render_kw={'placeholder': 'https://github.com/zoard-ehmann'})
+    submit = SubmitField('Add')
+
+
+class AdminLogin(FlaskForm):
+    email = EmailField('Email', validators=[InputRequired(), Email(message='This is not a valid email address.')], render_kw={'placeholder': 'your@email.com'})
+    password = PasswordField('Password', validators=[InputRequired()])
+    submit = SubmitField('Login')
